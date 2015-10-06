a0f173c662ac36a656c8e9445d9198c812e513e6
Andrew <andrewaguero@gmail.com>
2015-10-06 19:06:56 +0100

    built slider for transformable section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
-	// Get started!
-	
+var inkbox = document.getElementById("potato-rendered");
+var colorbox = document.getElementById("nonrendered");
+var fillerImage = document.getElementById("rendered");
+var handle = document.getElementById("handle");
+inkbox.addEventListener("mousemove",trackLocation,false);
+inkbox.addEventListener("touchstart",trackLocation,false);
+inkbox.addEventListener("touchmove",trackLocation,false);
+
+function trackLocation(e)
+{
+	var rect = fillerImage.getBoundingClientRect();
+	var position = ((e.pageX - rect.left) / fillerImage.offsetWidth)*100;
+	var position2 = ((e.pageX - rect.left -9) / fillerImage.offsetWidth)*100;
+	if (position <= 100) { colorbox.style.width = position+"%"; handle.style.left = position2+"%"; }
+}
+
 });
